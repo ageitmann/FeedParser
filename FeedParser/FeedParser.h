2021a8a5f7c9c0cc87b23814f551a469c2a27b8c
simutron IT-Service, Andreas Geitmann <geitmann@simutron.de>
2014-11-18 11:32:49 +0100

    Initial Commit

---
+++ FeedParser/FeedParser.h
@@ -0,0 +1,19 @@
+//
+//  FeedParser.h
+//  FeedParser
+//
+//  Created by Andreas Geitmann on 18.11.14.
+//  Copyright (c) 2014 simutron IT-Service. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FeedParser.
+FOUNDATION_EXPORT double FeedParserVersionNumber;
+
+//! Project version string for FeedParser.
+FOUNDATION_EXPORT const unsigned char FeedParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedParser/PublicHeader.h>
+
+
