aa7cb9e0326d77b53eb2f86ba90f9bbc35cd795d
simutron IT-Service, Andreas Geitmann <geitmann@simutron.de>
2014-11-20 11:35:57 +0100

    * Change "UIKit.h" to "Foundation.h" to provide usage with OS X

--- FeedParser/FeedParser.h
+++ FeedParser/FeedParser.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 simutron IT-Service. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for FeedParser.
 FOUNDATION_EXPORT double FeedParserVersionNumber;
